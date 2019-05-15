374159f39e0c77782e68bf48d7971a6091d8958c
Mason Sciotti <mason.sciotti@gmail.com>
2019-05-15 16:03:30 -0700

    Standing win64 rich presence test

---
+++ Assets/DiscordController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+public class DiscordController : MonoBehaviour {
+
+	public Discord.Discord discord;
+
+	// Use this for initialization
+	void Start () {
+		discord = new Discord.Discord(461618159171141643, (System.UInt64)Discord.CreateFlags.Default);
+		var activityManager = discord.GetActivityManager();
+		var activity = new Discord.Activity
+		{
+			State = "Still Testing",
+			Details = "Bigger Test"
+		};
+		activityManager.UpdateActivity(activity, (res) =>
+		{
+			if (res == Discord.Result.Ok)
+			{
+				Debug.LogError("Everything is fine!");
+			}
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		discord.RunCallbacks();
+	}
+}
